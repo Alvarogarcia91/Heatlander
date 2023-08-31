1c165bed636b02071492ff9af41ec6ca208b6333
Alvarogarcia91 <34041832+Alvarogarcia91@users.noreply.github.com>
2023-08-31 01:30:01 -0600

    mess with boostrap

--- heat/views.py
+++ heat/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 def index_view(request):
     return render(request, 'heat/index.html')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'heat/dashboard.html')
--- heatlanderproject/urls.py
+++ heatlanderproject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('heat.urls')),  # Incluye las URLs de la aplicación "Heat"
+
+    # Redireccionar a la página de inicio ('index') cuando se acceda a 'accounts/profile/'
+    path('accounts/profile/', lambda request: redirect('index'), name='profile_redirect'),
 ]
